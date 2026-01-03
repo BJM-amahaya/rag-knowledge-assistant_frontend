33e3e9598e0970c8c03eb7d21409066f442307ee
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-03 16:04:46 +0900

    カードタイル実装

--- lib/features/documents/views/document_tile.dart
+++ lib/features/documents/views/document_tile.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:rag_knowledge_assistant_frontend/features/documents/models/document.dart';
 
@@ -8,6 +7,33 @@ class DocumentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final sizeText = document.size >= 1048576
+        ? '${(document.size / 1048576).toStringAsFixed(1)}MB'
+        : '${(document.size / 1024).toStringAsFixed(1)}KB';
+
+    return Card(
+      color: Colors.blue, //後で調整
+      margin: EdgeInsets.all(20), //後で調整
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 3, horizontal: 12), //後で調整
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(Icons.text_snippet_outlined),
+            SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(document.name),
+                  Text(document.uploadedAt.toIso8601String().substring(0, 10)),
+                ],
+              ),
+            ),
+            Text(sizeText),
+          ],
+        ),
+      ),
+    );
   }
 }
