b9d7b8856a86c8df5457abf6281ffcf9f37af41c
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-06 21:33:23 +0900

    ドキュメントページ画面の実装

--- lib/features/documents/views/document_page.dart
+++ lib/features/documents/views/document_page.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rag_knowledge_assistant_frontend/features/documents/providers/document_provider.dart';
+import 'package:rag_knowledge_assistant_frontend/features/documents/views/document_tile.dart';
+import 'package:rag_knowledge_assistant_frontend/features/documents/views/upload_dialog.dart';
 
 class DocumentPage extends ConsumerWidget {
   const DocumentPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref
-        .watch(documentsProvider)
-        .when(
-          loading: () => CircularProgressIndicator(),
-          error: (err, stack) => Text('アップロードに失敗しました。'),
-          data: (documents) => ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: ,
+    return Scaffold(
+      appBar: AppBar(title: Text('ドキュメント')),
+      body: ref
+          .watch(documentsProvider)
+          .when(
+            loading: () => CircularProgressIndicator(),
+            error: (err, stack) => Text('アップロードに失敗しました。'),
+            data: (documents) => ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) {
+                return DocumentTile(document: documents[index]);
+              },
+            ),
           ),
-        );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(context: context, builder: (context) => UploadDialog());
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
