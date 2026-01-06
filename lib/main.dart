41cc15d1f67c071000d8651380f8c690cf60f2d5
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-06 22:04:00 +0900

    main.dart実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:rag_knowledge_assistant_frontend/features/documents/models/document.dart';
-import 'package:rag_knowledge_assistant_frontend/features/documents/views/document_tile.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rag_knowledge_assistant_frontend/core/router/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,53 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: DocumentTile(
-        document: Document(
-          id: '1',
-          name: 'テスト文書.pdf',
-          uploadedAt: DateTime.now(),
-          size: 1024000,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return MaterialApp.router(routerConfig: appRouter);
   }
 }
