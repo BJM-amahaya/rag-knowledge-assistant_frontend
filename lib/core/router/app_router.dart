8b722ffb4b64a524ea67007644076bb7685f1d35
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-14 00:05:25 +0900

    appRouter実装

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:rag_knowledge_assistant_frontend/features/documents/views/document_page.dart';
+import 'package:rag_knowledge_assistant_frontend/features/chat/views/chat_page.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/documents',
   routes: [
     GoRoute(path: '/documents', builder: (context, state) => DocumentPage()),
+    GoRoute(path: '/chat', builder: (context, state) => ChatPage()),
   ],
 );
